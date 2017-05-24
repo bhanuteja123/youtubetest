0e48704f3e325784fc0a1fdc23a2031008be258c
bhanuteja123 <bhanuteja1993@gmail.com>
2017-05-24 16:10:40 -0400

    test 1
    Signed-off-by: bhanuteja123 <bhanuteja1993@gmail.com>

--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -21,7 +21,5 @@ public class test {
         System.out.println("args = [" + args + "]");
         System.out.println("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj");
         System.out.println("bhanuteja");
-        System.out.println("args = [" + args + "]");
-        System.out.println("args = [" + args + "]");
     }
 }
