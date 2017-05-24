479fb8bb7982546fbbf5c9524969cbe4a32bfbcf
bhanuteja123 <bhanuteja1993@gmail.com>
2017-05-24 12:01:02 -0400

    My Final code
    Signed-off-by: bhanuteja123 <bhanuteja1993@gmail.com>

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -6,6 +6,6 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 /**
  * Created by bhanu_000 on 18-05-2017.
  */
-@CucumberOptions(features = "src\\test\\java\\feature", glue = "stepdefinition", tags = {"@History"}, dryRun = false, format = {"html:target/cucumber-htmlreport", "json:target/cucumber-report.json"})
+@CucumberOptions(features = "src\\test\\java\\feature", glue = "stepdefinition", tags = {"@Filter_options"}, dryRun = false, format = {"html:target/cucumber-htmlreport", "json:target/cucumber-report.json"})
 public class runner extends AbstractTestNGCucumberTests {
 }
--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -1,7 +1,12 @@
 package test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,12 +14,21 @@ import java.util.concurrent.TimeUnit;
  * Created by bhanu_000 on 18-05-2017.
  */
 public class test {
-    public static void main(String[] args) {
+
+    @Test
+    public static void main() {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.youtube.com/");
+        driver.get("https://www.google.com/search?q=gwt+tree&oq=gwt&aqs=chrome.0.69i59j69i57j69i60j0l2j69i60.970j0j7&sourceid=chrome&ie=UTF-8#q=gwt+");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
-        System.out.println("args = [" + args + "]");
+        System.out.println("fffffffff");
+        Actions ac= new Actions(driver);
+        WebElement we=driver.findElement(By.xpath("//div[3]/div/div[3]/div/div/h3/a"));
+        ac.moveToElement(we).click().build().perform();
+    }
+    @AfterTest
+    void t(){
+        System.out.println("ddddddd");
     }
 }
