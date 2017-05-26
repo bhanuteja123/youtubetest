f6ff02e7614159ac9209ad120a8a7600775b3291
bhanuteja123 <bhanuteja1993@gmail.com>
2017-05-26 13:39:37 -0400

    2 lines
    Signed-off-by: bhanuteja123 <bhanuteja1993@gmail.com>

--- src/test/java/test/Testjava.java
+++ src/test/java/test/Testjava.java
@@ -7,8 +7,19 @@ import java.util.Scanner;
  */
 public class Testjava {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter the value");
-        System.out.println(sc.nextLine());
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+        int number;
+        int avg;
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter a number:");
+            number = sc.nextInt();
+            total = total + number;
+        }
+        avg = total / 10;
+        System.out.println(avg);
+
     }
 }
