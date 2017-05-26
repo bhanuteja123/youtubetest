05ddc827fcff9ac1fc311c5298531ba47726c921
bhanuteja123 <bhanuteja1993@gmail.com>
2017-05-26 11:14:08 -0400

    2 lines
    Signed-off-by: bhanuteja123 <bhanuteja1993@gmail.com>

---
+++ src/test/java/test/Testjava.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+/**
+ * Created by bhanu_000 on 26-05-2017.
+ */
+public class Testjava {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the value");
+        System.out.println(sc.nextLine());
+    }
+}
